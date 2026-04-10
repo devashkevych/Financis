47a70edc86de80320f2e9bd0afa4fcddbfd5a901
TupacShampur <roma.ivashckevych@gmail.com>
2026-04-10 16:38:08 +0200

    WIP: add transactions component folder with addTransactionModal file and change transaction insert logic

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -4,33 +4,21 @@ import { supabase } from "@/lib/supabaseClient";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { User } from "@supabase/supabase-js";
-import { TransactionType, Transaction } from "@/types/transactions";
-import TransactionCard from "@/components/TransactionCard";
-
-interface FormState {
-  type: TransactionType;
-  amount: string;
-  category: string;
-  date: string;
-}
+import { Transaction } from "@/types/transactions";
+import TransactionCard from "@/components/transactions/TransactionCard";
+import AddTransactionModal from "@/components/transactions/AddTransactionModal";
 
 export default function TransactionsPage() {
   const router = useRouter();
-  const [form, setForm] = useState<FormState>({
-    type: "expense",
-    amount: "",
-    category: "Food",
-    date: new Date().toISOString().split("T")[0],
-  });
 
   const [transactions, setTransactions] = useState<Transaction[]>([]);
   const [isFetching, setIsFetching] = useState(true);
-  const [isSubmitting, setIsSubmitting] = useState(false);
   const [fetchingError, setFetchingError] = useState<string | null>(null);
-  const [submittingError, setSubmittingError] = useState<string | null>(null);
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
 
   const [user, setUser] = useState<User | null>(null);
 
+  console.log(isAddModalOpen);
   const sessionRetrieve = async () => {
     try {
       const {
@@ -60,31 +48,31 @@ export default function TransactionsPage() {
     sessionRetrieve();
   }, []);
 
-  useEffect(() => {
+  const fetchTransactions = async () => {
     if (!user) return;
 
     setIsFetching(true);
     setFetchingError(null);
 
-    const fetchTransactions = async () => {
-      try {
-        const { data, error } = await supabase
-          .from("Transactions")
-          .select("*")
-          .eq("user_id", user.id)
-          .order("date", { ascending: false });
+    try {
+      const { data, error } = await supabase
+        .from("Transactions")
+        .select("*")
+        .eq("user_id", user.id)
+        .order("date", { ascending: false });
 
-        if (error) throw error;
+      if (error) throw error;
 
-        setTransactions(data ?? []);
-      } catch (err) {
-        setFetchingError("Failed to fetch transactions");
-        return;
-      } finally {
-        setIsFetching(false);
-      }
-    };
+      setTransactions(data ?? []);
+    } catch (err) {
+      setFetchingError("Failed to fetch transactions");
+      return;
+    } finally {
+      setIsFetching(false);
+    }
+  };
 
+  useEffect(() => {
     fetchTransactions();
   }, [user]);
 
@@ -98,124 +86,33 @@ export default function TransactionsPage() {
     router.push("/auth/login");
   };
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    setIsSubmitting(true);
-    setSubmittingError(null);
-
-    try {
-      if (!user) {
-        setSubmittingError("User not authenticated");
-        return;
-      }
-
-      if (form.amount.trim() === "" || Number(form.amount) <= 0)
-        throw new Error("Wrong amount. Please write amount more than 0");
-
-      if (form.category.trim() === "")
-        throw new Error("Wrong category. Please write right category");
-
-      const payload = {
-        user_id: user.id,
-        date: form.date,
-        amount: Number(form.amount),
-        type: form.type,
-        category: form.category,
-      };
-
-      const { data: insertData, error: insertError } = await supabase
-        .from("Transactions")
-        .insert([payload])
-        .select();
-
-      if (insertError) throw new Error(insertError.message);
-      console.log(insertData);
-
-      setTransactions((prev) => [...prev, ...insertData]);
-      setForm({
-        type: "expense",
-        amount: "",
-        category: "",
-        date: new Date().toISOString().split("T")[0],
-      });
-    } catch (error) {
-      if (error instanceof Error) {
-        setSubmittingError(error.message);
-      } else {
-        setSubmittingError("Something went wrong");
-      }
-
-      console.error(error);
-    } finally {
-      setIsSubmitting(false);
-    }
-  };
-
   return (
-    <div>
+    <div className="relative h-screen">
+      {isAddModalOpen && (
+        <AddTransactionModal
+          userId={user?.id}
+          onClose={() => {
+            setIsAddModalOpen(false);
+          }}
+          onSuccess={fetchTransactions}
+        />
+      )}
       <h1>Transactions</h1>
       <button onClick={handleSignOut}>Sign Out</button>
-      <form onSubmit={handleSubmit} className="flex flex-wrap gap-4 mt-8">
-        {/* TYPE */}
-        <div className="flex gap-4">
-          <label htmlFor="type">Type</label>
-          <select
-            id="type"
-            value={form.type}
-            onChange={(e) => {
-              setForm({ ...form, type: e.target.value as TransactionType });
-            }}
-          >
-            <option value="expense">Expense</option>
-            <option value="income">Income</option>
-          </select>
-        </div>
-        {/* AMOUNT */}
-        <div className="flex gap-4">
-          <label htmlFor="amount">Amount</label>
-          <input
-            id="amount"
-            type="number"
-            value={form.amount}
-            onChange={(e) => {
-              setForm({ ...form, amount: e.target.value });
-            }}
-            placeholder="0.00"
-          />
-        </div>
-        {/* Category */}
-        <div className="flex gap-4">
-          <label htmlFor="category">Category</label>
-          <select
-            id="category"
-            value={form.category}
-            onChange={(e) => {
-              setForm({ ...form, category: e.target.value });
-            }}
-          >
-            <option value="Food">Food</option>
-            <option value="Transport">Transport</option>
-            <option value="Rent">Rent</option>
-          </select>
-        </div>
-        {/* Date */}
-        <input
-          type="date"
-          value={form.date}
-          onChange={(e) => {
-            setForm({ ...form, date: e.target.value });
-          }}
-        />
-        <button type="submit">Add</button>
-      </form>
+      <button
+        className="border rounded p-4"
+        onClick={() => {
+          setIsAddModalOpen(true);
+        }}
+      >
+        +
+      </button>
+
       {isFetching && <p>Loading your transactions...</p>}
       {fetchingError && <p>{fetchingError}</p>}
       {!isFetching && !fetchingError && transactions.length === 0 && (
         <p>No transactions yet...</p>
       )}
-      {submittingError && <p>{submittingError}</p>}
-      {}
       {!isFetching && !fetchingError && transactions.length > 0 && (
         <ul className="flex flex-col mt-4">
           {transactions.map((transaction) => (
---
+++ components/transactions/AddTransactionModal.tsx
@@ -0,0 +1,142 @@
+import { useState } from "react";
+import { TransactionType } from "@/types/transactions";
+import { supabase } from "@/lib/supabaseClient";
+
+interface FormState {
+  type: TransactionType;
+  amount: string;
+  category: string;
+  date: string;
+}
+
+interface AddTransactionModalProps {
+  userId: string | undefined;
+  onClose: () => void;
+  onSuccess: () => Promise<void>;
+}
+
+export default function AddTransactionModal({
+  userId,
+  onClose,
+  onSuccess,
+}: AddTransactionModalProps) {
+  const [form, setForm] = useState<FormState>({
+    type: "expense",
+    amount: "",
+    category: "Food",
+    date: new Date().toISOString().split("T")[0],
+  });
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [submittingError, setSubmittingError] = useState<string | null>(null);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setIsSubmitting(true);
+    setSubmittingError(null);
+
+    try {
+      if (form.amount.trim() === "" || Number(form.amount) <= 0)
+        throw new Error("Wrong amount. Please write amount more than 0");
+
+      const payload = {
+        user_id: userId,
+        date: form.date,
+        amount: Number(form.amount),
+        type: form.type,
+        category: form.category,
+      };
+
+      const { data: insertData, error: insertError } = await supabase
+        .from("Transactions")
+        .insert([payload])
+        .select();
+
+      if (insertError) throw new Error(insertError.message);
+
+      onClose();
+      onSuccess();
+
+      setForm({
+        type: "expense",
+        amount: "",
+        category: "",
+        date: new Date().toISOString().split("T")[0],
+      });
+    } catch (error) {
+      if (error instanceof Error) {
+        setSubmittingError(error.message);
+      } else {
+        setSubmittingError("Something went wrong");
+      }
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  return (
+    <div className="absolute z-2 bg-slate-700 h-88 px-8  rounded-xl">
+      <form onSubmit={handleSubmit} className="flex flex-wrap gap-4 mt-8">
+        {/* TYPE */}
+        <div className="flex gap-4 border-b w-full">
+          <label htmlFor="type">Type:</label>
+          <select
+            id="type"
+            value={form.type}
+            onChange={(e) => {
+              setForm({ ...form, type: e.target.value as TransactionType });
+            }}
+          >
+            <option value="expense">Expense</option>
+            <option value="income">Income</option>
+          </select>
+        </div>
+        {/* AMOUNT */}
+        <div className="flex gap-4 border-b w-full">
+          <label htmlFor="amount">Amount:</label>
+          <input
+            id="amount"
+            type="number"
+            value={form.amount}
+            onChange={(e) => {
+              setForm({ ...form, amount: e.target.value });
+            }}
+            placeholder="0.00"
+          />
+        </div>
+        {/* Category */}
+        <div className="flex gap-4 border-b w-full">
+          <label htmlFor="category">Category:</label>
+          <select
+            id="category"
+            value={form.category}
+            onChange={(e) => {
+              setForm({ ...form, category: e.target.value });
+            }}
+          >
+            <option value="Food">Food</option>
+            <option value="Transport">Transport</option>
+            <option value="Rent">Rent</option>
+          </select>
+        </div>
+        {/* Date */}
+        <div className="flex w-full gap-4 border-b">
+          <label htmlFor="date">Date:</label>
+          <input
+            id="date"
+            type="date"
+            value={form.date}
+            onChange={(e) => {
+              setForm({ ...form, date: e.target.value });
+            }}
+          />
+        </div>
+        <button className="mx-auto px-8 py-4 rounded-xl bg-teal-700 mt-4" type="submit">
+          Add
+        </button>
+      </form>
+      {submittingError && <p className="mt-4">{submittingError}</p>}
+    </div>
+  );
+}
