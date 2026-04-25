dd232590d7ba04f9167af913d3fa74b2b7cd6ca2
TupacShampur <roma.ivashckevych@gmail.com>
2026-04-25 12:27:42 +0200

    ADD: implement calculations and latestTransactions on Dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,14 +5,24 @@ import { supabase } from "@/lib/supabaseClient";
 import { useRouter } from "next/navigation";
 import calculateTransactionSummary from "@/features/dashboard/utils/calculateTransactionSummary";
 import useTransactions from "@/features/transactions/hooks/useTransactions";
+import TransactionCard from "@/features/transactions/components/TransactionCard";
 
 export default function DashboardPage() {
-  const { user, isLoading } = useRequireAuth();
-  const { transactions } = useTransactions(user?.id);
+  const { user, isLoading: authLoading } = useRequireAuth();
+
+  const {
+    transactions,
+    isFetching: transactionsLoading,
+    deleteTransaction,
+  } = useTransactions(user?.id);
+
+  const isLoading = authLoading || transactionsLoading;
   const router = useRouter();
 
   const { totalBalance, totalIncomes, totalExpenses } =
     calculateTransactionSummary(transactions);
+
+  const latestTransactions = transactions.slice(0, 5);
 
   const handleSignOut = async () => {
     const { error } = await supabase.auth.signOut();
@@ -27,9 +37,21 @@ export default function DashboardPage() {
     <div>
       <h1>Dashboard</h1>
       <button onClick={handleSignOut}>Sign Out</button>
-      <div>Total Balance: {totalBalance}</div>
-      <div>Total Incomes: {totalIncomes}</div>
-      <div>Total Expenses: {totalExpenses}</div>
+      {isLoading && <p>Loading...</p>}
+      {!isLoading && (
+        <div>
+          <div>Total Balance: {totalBalance}</div>
+          <div>Total Incomes: {totalIncomes}</div>
+          <div>Total Expenses: {totalExpenses}</div>
+          {latestTransactions.map((t) => (
+            <TransactionCard
+              key={t.id}
+              transaction={t}
+              onDelete={deleteTransaction}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
